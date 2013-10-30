8d27940a618131c8aef4eec123e59bd7eaa4ecd3
klacointe <kevinlacointe@gmail.com>
2013-10-30 10:39:13 +0100

    Initial commit (Rails app)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GrapeSwagger::Application.config.secret_key_base = '1c2b0ecf68cef8e77d5a96a83ee7a4892ab30e5ec1f29f5f77f2d323a38089b3c519754f0acfb362be6491d9cce3be1786dd1ed44424e467985c560470e45859'
